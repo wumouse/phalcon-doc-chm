bb8380ab14effe7eaac0553daf6b1d3f975eb8cb
wumouse <wumouse@qq.com>
2015-11-17 10:41:09 +0800

    update README

--- README.md
+++ README.md
@@ -11,13 +11,13 @@ usage:
 
     Just use --auto if you don't know the procedure.
 
-    ----auto      execute all task in the procedure automatically
-    ------------  -----------------------------------------------------
-    ----backup    backup html
-    ----restore   restore from backup
-    ----toUtf8    convert encoding from gb2312 to utf-8
-    ----toGb2312  convert encoding from utf-8 to gb2312
-    ----tidy      tidy incomplete html
-    ----main      parse html dom , and then remove some tag cause document load slowly, parse and generate index for chm
-    ----clean     clean the sign files, but execute always before and after the loop
-    ----save      flush the result into file
+    --auto      execute all task in the procedure automatically
+    ----------  -----------------------------------------------------
+    --backup    backup html
+    --restore   restore from backup
+    --toUtf8    convert encoding from gb2312 to utf-8
+    --toGb2312  convert encoding from utf-8 to gb2312
+    --tidy      tidy incomplete html
+    --main      parse html dom , and then remove some tag cause document load slowly, parse and generate index for chm
+    --clean     clean the sign files, but execute always before and after the loop
+    --save      flush the result into file
--- src/Dispatcher.php
+++ src/Dispatcher.php
@@ -67,17 +67,17 @@ class Dispatcher
     public function getOptionsDescription()
     {
         $descriptions = array(
-            '--auto' => 'execute all task in the procedure automatically',
-            '--------------------' => '-----------------------------------------------------',
-            '--backup' => 'backup html',
-            '--restore' => 'restore from backup',
-            '--toUtf8' => 'convert encoding from gb2312 to utf-8',
-            '--toGb2312' => 'convert encoding from utf-8 to gb2312',
-            '--tidy' => 'tidy incomplete html',
-            '--main' => 'parse html dom , and then remove some tag cause document' .
+            'auto' => 'execute all task in the procedure automatically',
+            '------------------' => '-----------------------------------------------------',
+            'backup' => 'backup html',
+            'restore' => 'restore from backup',
+            'toUtf8' => 'convert encoding from gb2312 to utf-8',
+            'toGb2312' => 'convert encoding from utf-8 to gb2312',
+            'tidy' => 'tidy incomplete html',
+            'main' => 'parse html dom , and then remove some tag cause document' .
                 ' load slowly, parse and generate index for chm',
-            '--clean' => 'clean the sign files, but execute always before and after the loop',
-            '--save' => 'flush the result into file'
+            'clean' => 'clean the sign files, but execute always before and after the loop',
+            'save' => 'flush the result into file'
         );
 
         $response = 'Notice: The order of arguments passed is the execute order' . PHP_EOL . PHP_EOL .
